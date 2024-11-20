8cdc045678130463ea62ee646ab7323f9b3b2c98
Lars Gielen <larsgielen2003@gmail.com>
2024-11-20 13:47:59 +0100

    Add Command line variables for all parameters

--- src/cuda/main.cpp
+++ src/cuda/main.cpp
@@ -19,6 +19,63 @@ int main(int argc, char* argv[]) {
         .help("Enable verbose output")
         .default_value(false)
         .implicit_value(true);
+        
+    args.add_argument("--frameWidth")
+        .help("")
+        .default_value("");
+        
+    args.add_argument("--frameHeight")
+        .help("")
+        .default_value("");
+        
+    args.add_argument("--frameAmount")
+        .help("")
+        .default_value("");
+        
+    args.add_argument("--iteration_per_frame")
+        .help("")
+        .default_value("");
+        
+    args.add_argument("--deltaTime")
+        .help("")
+        .default_value("");
+        
+    args.add_argument("--kernel_radius")
+        .help("")
+        .default_value("");
+        
+    args.add_argument("--kernel_alpha")
+        .help("")
+        .default_value("");
+        
+    args.add_argument("--kernel_peak_heights")
+        .help("")
+        .default_value("");
+        
+    args.add_argument("--growth_mhu")
+        .help("")
+        .default_value("");
+        
+    args.add_argument("--growth_sigma")
+        .help("")
+        .default_value("");
+        
+    args.add_argument("--blocks_x")
+        .help("")
+        .default_value("");
+        
+    args.add_argument("--blocks_y")
+        .help("")
+        .default_value("");
+        
+    args.add_argument("--threads_x")
+        .help("")
+        .default_value("");
+        
+    args.add_argument("--threads_y")
+        .help("")
+        .default_value("");
+
 
     try {
         args.parse_args(argc, argv);
@@ -44,37 +101,62 @@ int main(int argc, char* argv[]) {
     int threads_x;
     int threads_y;
 
+    nlohmann::json config;
     if (args.get<std::string>("--input") != "") {
         std::ifstream file(args.get<std::string>("--input"));
         if (!file.is_open()) {
             std::cerr << "Error opening config file" << std::endl; 
             return 1;
         }
-        
-        nlohmann::json config;
         file >> config;
-        frameWidth = config["frameWidth"].get<int>();
-        frameHeight = config["frameHeight"].get<int>();
-        frameAmount = config["frameAmount"].get<int>();
-
-        iterationPerFrame = config["iteration_per_frame"].get<int>();
-
-        deltaTime = config["deltaTime"].get<float>();
-        kernelRadius = config["kernel_radius"].get<int>();
-        kernelAlpha = config["kernel_alpha"].get<float>(); 
-        kernelPeakHeights = config["kernel_peak_heights"].get<std::vector<float>>();
-        growthMhu = config["growth_mhu"].get<float>();
-        growthSigma = config["growth_sigma"].get<float>();
-
-        blocks_x = config["blocks_x"].get<int>();
-        blocks_y = config["blocks_y"].get<int>();
-        threads_x = config["threads_x"].get<int>();
-        threads_y = config["threads_y"].get<int>();
     }
     else {
-        std::cerr << "No input file given." << std::endl; 
-        return 1;
+        config["frameWidth"] = std::stoi(args.get<std::string>("--frameWidth"));
+        config["frameHeight"] = std::stoi(args.get<std::string>("--frameHeight"));
+        config["frameAmount"] = std::stoi(args.get<std::string>("--frameAmount"));
+
+        config["iteration_per_frame"] = std::stoi(args.get<std::string>("--iteration_per_frame"));
+
+        config["deltaTime"] = std::stof(args.get<std::string>("--deltaTime"));
+        config["kernel_radius"] = std::stoi(args.get<std::string>("--kernel_radius"));
+        config["kernel_alpha"] = std::stof(args.get<std::string>("--kernel_alpha"));
+
+        std::string peaksStr = args.get<std::string>("--kernel_peak_heights");
+        std::istringstream peaksStream(peaksStr);
+        std::string peak;
+        std::vector<float> kernelPeaks;
+        while (std::getline(peaksStream, peak, ',')) {
+            kernelPeaks.push_back(std::stof(peak));
+        }
+        config["kernel_peak_heights"] = kernelPeaks;
+
+        config["growth_mhu"] = std::stof(args.get<std::string>("--growth_mhu"));
+        config["growth_sigma"] = std::stof(args.get<std::string>("--growth_sigma"));
+
+        config["blocks_x"] = std::stoi(args.get<std::string>("--blocks_x"));
+        config["blocks_y"] = std::stoi(args.get<std::string>("--blocks_y"));
+        config["threads_x"] = std::stoi(args.get<std::string>("--threads_x"));
+        config["threads_y"] = std::stoi(args.get<std::string>("--threads_y"));
     }
+
+    
+    frameWidth = config["frameWidth"].get<int>();
+    frameHeight = config["frameHeight"].get<int>();
+    frameAmount = config["frameAmount"].get<int>();
+
+    iterationPerFrame = config["iteration_per_frame"].get<int>();
+
+    deltaTime = config["deltaTime"].get<float>();
+    kernelRadius = config["kernel_radius"].get<int>();
+    kernelAlpha = config["kernel_alpha"].get<float>(); 
+    kernelPeakHeights = config["kernel_peak_heights"].get<std::vector<float>>();
+    growthMhu = config["growth_mhu"].get<float>();
+    growthSigma = config["growth_sigma"].get<float>();
+
+    blocks_x = config["blocks_x"].get<int>();
+    blocks_y = config["blocks_y"].get<int>();
+    threads_x = config["threads_x"].get<int>();
+    threads_y = config["threads_y"].get<int>();
 
     LeniaData data(
         frameWidth, frameHeight, frameAmount, iterationPerFrame,
